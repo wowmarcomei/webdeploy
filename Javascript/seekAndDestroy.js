679031ec7a5ae110e12b953364efb85022b98334
wowmarcomei <wowmarcomei@gmail.com>
2016-09-09 22:29:21 +0630

    ...

---
+++ Javascript/seekAndDestroy.js
@@ -0,0 +1,17 @@
+/**
+ * Created by meixuhong on 9/9/16.
+ */
+
+function seekElement(element) {
+
+}
+
+function destroyer(arr) {
+    // Remove all the values
+
+
+
+    return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
