b476fa6f7a87dc6db6f6b5875d14ed260385a9cc
wowmarcomei <wowmarcomei@gmail.com>
2017-03-23 08:14:23 +0630

    ...

---
+++ exercise/7_PythonSpider/week1/1.3_real_webpage_parse/parse_tripadvisor.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://cn.tripadvisor.com/Attractions-g60763-Activities-New_York_City_New_York.html'
+wb_data = requests.get(url)
+soup = BeautifulSoup(wb_data.text,'lxml')
+print(soup)
