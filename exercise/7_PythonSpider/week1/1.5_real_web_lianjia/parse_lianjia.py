c089415189d3eeda00cf64aaa07c27dded8d45bd
wowmarcomei <wowmarcomei@gmail.com>
2017-04-06 08:15:53 +0630

    增加链家解析

--- exercise/7_PythonSpider/week1/1.5_real_web_lianjia/parse_lianjia.py
+++ exercise/7_PythonSpider/week1/1.5_real_web_lianjia/parse_lianjia.py
@@ -6,28 +6,42 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
 }
 
+headers_m = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+}
+
 proxies = {"https":"https://191.96.43.23:8080"}
 
 base_urls = ['http://sz.lianjia.com/chengjiao/pg{}/'.format(str(i)) for i in range(1,101,1)]
 
+base_info = {}
 
-wb_data = requests.get(base_urls[0],headers=headers,proxies=proxies)
-soup = BeautifulSoup(wb_data.text,'lxml')
+def get_house_name_link(url):
+    wb_data = requests.get(url,headers=headers,proxies=proxies)
+    soup = BeautifulSoup(wb_data.text,'lxml')
 
-titles = soup.select('ul.listContent > li > div.info > div.title > a')
-deal_dates = soup.select('div.dealDate')
-total_prices = soup.select('div.totalPrice > span.number')
-original_prices = soup.select('div.dealCycleeInfo > span.dealCycleTxt > span:nth-of-type(1)')
-unit_prices = soup.select('div.unitPrice > span.number')
-deal_cycle_dates = soup.select('div.dealCycleeInfo > span.dealCycleTxt > span:nth-of-type(2)')
+    titles = soup.select('ul.listContent > li > div.info > div.title > a')
+    links = soup.select('div.info > div.title > a')
 
-for title,deal_date,total_price,original_price,unit_price,deal_cycle_date in zip(titles,deal_dates,total_prices,original_prices,unit_prices,deal_cycle_dates):
-    data = {
-        'title':title.get_text(),
-        'deal_date':deal_date.get_text(),
-        'total_price':total_price.get_text(),
-        'original_price':original_price.get_text(),
-        'unit_price':unit_price.get_text(),
-        'deal_cycle_date':deal_cycle_date.get_text()
-    }
-    print(data)
+    for title,link, in zip(titles,links):
+        # 插入字典数据
+        base_info['{}'.format(title.get_text().split()[0])] = '{}'.format(link.get('href').split('/')[-1])
+    print(base_info)
+
+def get_house_name_links(pages):
+    for i in range(0,pages,1):
+        get_house_name_link(base_urls[i])
+        print('\n\nDone {}\n\n'.format(i))
+
+# get_house_name_links(10)
+
+def get_house_info(url):
+    wb_data = requests.get(url,headers=headers_m,proxies=proxies)
+    soup = BeautifulSoup(wb_data.text,'lxml')
+
+    a = soup.select('div > a.btn.btn_gray.post_ulog')[0].get('href')
+    # a = soup.find_all('a',class_='getMoreHouse')
+    print(a)
+
+get_house_info('https://m.lianjia.com/sz/chengjiao/105100678720.html')
+# get_house_info('http://sz.lianjia.com/chengjiao/105100688335.html')
