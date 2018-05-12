c23165ff1f8c6f7c906c9f03a50ce0a25942bbed
Meixuhong (Marco) <m00240582@china.huawei.com>
2018-05-12 22:40:13 +0630

    定制admin

--- python_web/django_test/mysite/settings.py
+++ python_web/django_test/mysite/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Django能查找工程中任何以templates命名的目录
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
