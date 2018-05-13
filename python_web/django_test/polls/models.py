5fc5d2f6c633c33852559cf818cd0e076bbe9ce7
Meixuhong (Marco) <m00240582@china.huawei.com>
2018-05-13 18:10:36 +0630

    理解GeneralView

--- python_web/django_test/polls/models.py
+++ python_web/django_test/polls/models.py
@@ -25,3 +25,18 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    class Meta:
+        ordering = ["-name"]
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
--- python_web/django_test/polls/views.py
+++ python_web/django_test/polls/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
-from polls.models import Question, Choice
+from polls.models import Question, Choice, Publisher
 from django.urls import reverse
 from django.views import generic
+from django.views.generic import TemplateView,ListView
 
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
+# def index(request):
     # 获取Question对象后进行排序：按照时间倒叙，且只获取前5个
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {}
-    context['latest_question_list'] = latest_question_list
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {}
+    # context['latest_question_list'] = latest_question_list
 
-    return render(request, 'polls/index.html', context)
+    # return render(request, 'polls/index.html', context)
 
 # question_id从URL的参数中传递过来
-def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request,question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
 
 
 # question_id从URL的参数中传递过来
@@ -46,27 +47,53 @@ def vote(request,question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 # question_id从URL的参数中传递过来
-def results(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 # 下面使用GenericView
-# class IndexView(generic.ListView):
-#     template_name = 'polls/index.html'
-#     context_object_name = 'latest_question_list'
+class IndexView(generic.ListView):
+    '''
+    下面的属性与方法均为继承父类ListView
+    '''
+    # 表示这个view将会使用哪个模板渲染
+    template_name = 'polls/index.html'
+    # 标识模板中使用的上下文context对象为：latest_question_list会被传入html模板中引用
+    # 而context对象本身则从下面的方法get_queryset中获取
+    context_object_name = 'latest_question_list'
 
-#     def get_queryset(self):
-#         """Return the last five published questions."""
-#         return Question.objects.order_by('-pub_date')[:5]
+    # 重写get_queryset函数，返回值为QuerySet,对应为上面的context_object_name
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/detail.html'
+    # 上面的写法完全等同于下面四句
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {}
+    # context['latest_question_list'] = latest_question_list
+    # return render(request, 'polls/index.html', context)
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-# class ResultsView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/results.html'
+# def detail(request,question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+class AboutView(generic.TemplateView):
+    template_name = 'polls/about.html'
+
+class PublisherView(ListView):
+    model = Publisher
+    template_name = 'polls/about.html'
